0eb5f66fe3551b9bbdb8da005087608949fa8754
Max Brodeur <maxbrod21@gmail.com>
2022-12-22 17:03:59 -0500

    Parameters bar

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 const canvas = document.querySelector("canvas");
 const width = canvas.width = window.innerWidth
 const height = canvas.height = window.innerHeight
-
 const ctx = canvas.getContext("2d")
+
+// PARAMETERS
+const close_button = document.querySelector(".close_button")
+const parameters = document.querySelector(".parameters")
+const run_button = document.querySelector(".run_button");
+// sliders
+const speed_slider = document.querySelector(".speed")
+const speed_output = document.querySelector("#speed")
+const max = speed_slider.max
+const res_slider = document.querySelector(".resolution")
+const res_output = document.querySelector("#resolution")
+
+
+// INITIAL SCREEN
+ctx.fillStyle = "rgb(0,0,0)"
+ctx.fillRect(0,0,width,height)
+
+var cells_matrix 
+var COLS,ROWS, num_cells, speed, CELL_HEIGHT, CELL_WIDTH
+
+function init(){
+
+    // INITIAL VALUES
+    num_cells = document.querySelector(".num_cells").value
+    // ROWS = document.querySelector(".num_rows").value
+    // COLS = document.querySelector(".num_cols").value
+    speed = document.querySelector(".speed").value
+    resolution = document.querySelector(".resolution").value
+    ROWS = Math.floor(height*(resolution/100.0))
+    COLS = Math.floor(width*(resolution/100))
+    CELL_WIDTH = width/COLS
+    CELL_HEIGHT = height/ROWS
+
+    gen_cells()
+    
+}
 
 const ALIVE_COLOR = "rgb(255,255,255)"
 const DEAD_COLOR = "rgb(0,0,0)"
-const COLS = 80
-const ROWS = 50
-const CELL_WIDTH = width/COLS
-const CELL_HEIGHT = height/ROWS
-
-// MATRIX CONTAINING CELLS
-const cell_array = new Array(ROWS)
-for (let i = 0; i < ROWS; i++) {
-    cell_array[i] = new Array(COLS)
-}
 
 const STEPS = 10
 
@@ -29,14 +54,12 @@ class Cell {
     }
     
     draw() {
-        // ctx.beginPath()
         this.color = DEAD_COLOR
         if (this.alive===1) {
             this.color = ALIVE_COLOR
         }
         ctx.fillStyle = this.color
         ctx.fillRect(this.x*CELL_WIDTH,this.y*CELL_HEIGHT,CELL_WIDTH,CELL_HEIGHT)
-        // ctx.fill()
     }
 
     neighbor_count(){
@@ -56,7 +79,7 @@ class Cell {
         let ctr = 0
         for (let i = 0; i < indices.length; i++) {
             const index = indices[i];
-            if (cell_array[index[0]][index[1]]===1){
+            if (cells_matrix[index[0]][index[1]].alive===1){
                 ctr += 1
             }   
         }
@@ -78,12 +101,14 @@ class Cell {
                 this.alive = 1
             }
         }
-        cell_array[this.y][this.x]=this.alive
     }
 
     revive() {
         this.alive=1
-        cell_array[this.y][this.x] = 1
+    }
+
+    kill() {
+        this.alive=0
     }
 
 } 
@@ -91,60 +116,99 @@ class Cell {
 
 
 function gen_cells(){
-    const cells = []
-    for (let i = 0; i < cell_array.length; i++) {
-        const r = cell_array[i];
-        for (let j = 0; j < r.length; j++) {
-            cells.push(new Cell(j,i))
+    cells_matrix = []
+    for (let i = 0; i < ROWS; i++) {
+        cells_matrix[i] = []
+        for (let j = 0; j < COLS; j++) {
+            let cell = new Cell(j,i)
+            cells_matrix[i][j] = cell
         }
     }
-    return cells
+}
+
+// function kill_all(cells){
+//     cells.forEach(cell=>cell.kill())
+// }
+
+function resize_cells(res){
+    
+    
+    let old_ROWS = ROWS
+    let old_COLS = COLS
+    
+    resolution = res
+    ROWS = Math.floor(height*(resolution/100.0))
+    COLS = Math.floor(width*(resolution/100))
+    CELL_WIDTH = width/COLS
+    CELL_HEIGHT = height/ROWS
+    
+    
+    // if shrinking, bound is new value, if growing, bound is old value
+    let row_bound = (old_ROWS <= ROWS) ? old_ROWS : ROWS
+    let col_bound = (old_COLS <= COLS) ? old_COLS : COLS
+
+    var old_cells_matrix = cells_matrix
+    // generate new cells
+    gen_cells()
+
+    // fill with old
+    for (let i = 0; i < row_bound; i++) {
+        for (let j = 0; j < col_bound; j++) {
+            cells_matrix[i][j] = old_cells_matrix[i][j]
+        }
+    }
 }
 
 
-function revive_random(num, cells_){
+function revive_random(){
     let blacklist = []
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < num_cells; i++) {
         if (blacklist.length>=COLS*ROWS){
             console.log("ERROR: too many cells")
             return;
         }
-        let index = Math.floor(Math.random()*COLS*ROWS)
+        let index = [Math.floor(Math.random()*ROWS), Math.floor(Math.random()*COLS)]
         while(blacklist.includes(index)){
-            index = Math.floor(Math.random()*COLS*ROWS)
+            index = [Math.floor(Math.random()*ROWS), Math.floor(Math.random()*COLS)]
         }
         blacklist.push(index)
-        let cell = cells_[index]
+        let cell = cells_matrix[index[0]][index[1]]
         cell.revive()
     }
 }
 
-const cells = gen_cells()
-revive_random(200,cells)
-
-// midX = width/2.0-CELL_WIDTH/2.0
-// midY = height/2.0-CELL_HEIGHT/2.0
-// const cell = new Cell(midX,midY)
-
-// const cells = random_cells(200)
 
 
 
-let count = 0
+function run() {
+    init()
+    revive_random()    
+    loop()
+}
+
+var count = 0
+
 
 function loop() {
+
+    speed = max - (speed_slider.value-1)   
+
+    let res = res_slider.value
+    while(res_slider.value!==resolution){resize_cells(res_slider.value)}
     
-    if(count%20==0){
+    if(count%speed==0){
         
         //background
         ctx.fillStyle = "rgb(0,0,0)"
         ctx.fillRect(0,0,width,height)
 
         //cells
-        cells.forEach(cell => {
-            cell.draw()
-            cell.update()            
-        });
+        for (let i = 0; i < ROWS; i++) {
+            for (let j = 0; j < COLS; j++) {
+                cells_matrix[i][j].draw()
+                cells_matrix[i][j].update()
+            }
+        }
 
         count=0        
     }
@@ -153,15 +217,36 @@ function loop() {
     requestAnimationFrame(loop)
 }
 
-// function loop(){
-//     ctx.fillStyle = "rgb(0,0,0)"
-//     ctx.fillRect(0,0,width,height)
-//     for (const cell of cells) {
-//         cell.draw()
-//         cell.update()
-//     }
+run_button.onclick = run
+run()
 
-// }
 
-loop()
+// FORMATTING
 
+close_button.onclick = function () {
+    parameters.style.display = "None"
+}
+
+canvas.onclick = function () {
+    parameters.style.display = "flex"
+}
+
+
+
+function res_text_update() {
+    res_output.innerText = res_slider.value + "%"
+}
+
+res_text_update()
+res_slider.onchange = res_text_update
+
+
+function speed_text_update() {
+    speed_output.innerText = max - speed_slider.value
+}
+speed_slider.onchange = speed_text_update
+
+
+
+// Initial display
+speed_text_update()
